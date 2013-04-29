3c49eebe52e133fb4229a2ce82955e4a103b5015
Sebastien PRUNIER <sebastien.prunier@gmail.com>
2013-04-29 17:03:25 +0200

    fixing jshint warnings :-)

--- server/src/core/geeksRepository.js
+++ server/src/core/geeksRepository.js
@@ -4,13 +4,13 @@ var GeeksRepository = function() {
 	var _insert = function(geek, callback) {
 		// TODO insert into mongo collection
 		callback();
-	}
+	};
 
 	var _find = function(query, callback) {
 		// TODO find geeks from mongo collection
 		var geeks = [{"name":"geek1"},{"name":"geek2"}];
 		callback(geeks);
-	}
+	};
 	
 	return {
 		insert : _insert,
