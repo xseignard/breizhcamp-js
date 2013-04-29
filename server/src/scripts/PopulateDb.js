fd3a770ab40c85eaa2628eee2ca82d016b9efe36
Sebastien PRUNIER <sebastien.prunier@gmail.com>
2013-04-29 18:06:02 +0200

    adding async for PopulateDb script

--- server/src/scripts/PopulateDb.js
+++ server/src/scripts/PopulateDb.js
@@ -1,6 +1,7 @@
 'use strict';
 // require
-var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient,
+	async = require('async');
 
 // mongodb uri
 var uri = require('../conf/conf').MONGO_URL;
@@ -10,17 +11,33 @@ var geeks = require('./geeks.json');
 
 MongoClient.connect(uri, function(err, db) {
 	db.collection('geeks', function(err, collection) {
-		// remove the collection (if existing)
-		collection.remove({},function(err, removed){});
-		// create it
-		collection.insert(geeks, {safe : true},
-			function(err, result) {
+		async.series(
+			[
+				// 1- remove the collection (if existing)
+				function(callback) {
+					collection.remove({}, function(err, removed){
+						callback(err, removed + " geek(s) removed !");
+					});
+				},
+        
+				// 2- insert geeks !
+				function(callback) {
+					collection.insert(geeks, {safe : true},	function(err, result) {
+						callback(err, result.length + " geek(s) inserted !");
+					});
+				}
+			],
+			// final callback function
+			function(err, results) {
 				if (err) {
 					console.log('Something went wrong');
 					process.exit(1);
 				}
-				console.log('Done');
+				results.map(function(result) {
+					console.log(result);
+				});
 				process.exit(0);
-			});
+			}
+		);
 	});
 });
