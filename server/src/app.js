9e4e89bd7edef7d2af3d85481c4f02e6834172de
sebprunier <sebastien.prunier@gmail.com>
2013-05-30 15:48:44 +0200

    adding shutdown hook

--- server/src/app.js
+++ server/src/app.js
@@ -8,20 +8,23 @@ var express = require('express'),
 // configure geeks repository
 var geeksRepository = new GeeksRepository(conf.MONGO_URL, 'geeks');
 geeksRepository.connect();
+
 // configure routes
 var routes = new GeeksRoutes(geeksRepository);
 app.post('/geek', routes.create);
 app.get('/geek/likes/:like', routes.likes);
+
+// shutdown hook
+var cleanup = function () {
+	console.log('geeks-backend is shutting down...');
+	geeksRepository.close();
+	process.exit();
+};
+process.on('SIGTERM', cleanup);
+process.on('SIGINT', cleanup);
 
 // start server
 var port = process.env.PORT || 9999;
 app.listen(port);
 console.log('geeks-backend listening on port %s', port);
 
-// TODO shutdown hook
-/*
-process.on('SIGTERM', function () {
-	console.log('geeks-backend is shutting down...');
-	geeksRepository.close();
-});
-*/
